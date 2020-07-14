bda1b531dd90e2ccaaa31351bf827d525b383d7c
Kyle Jordan <kjord021@fiu.edu>
2020-07-14 11:57:02 -0400

    Add Lodash and Finish Custom List Implementation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -28,7 +29,7 @@ const item2 = new Item ({
 });
 
 const item3 = new Item ({
-  name: "<-- Hit this to delete an item."
+  name: "← Hit this to delete an item."
 });
 
 const defaultItems = [item1, item2, item3];
@@ -71,7 +72,7 @@ app.get("/", function(req, res) {
 
 app.get("/:customListName", function(req,res){
 
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name: customListName}, function(err, foundList){
     if(!err){
@@ -120,15 +121,27 @@ app.post("/", function(req,res){
 
 app.post("/delete", function(req, res){
   const checkedItemId = (req.body.checkbox);
+  const listName = req.body.listName;
 
-  Item.findByIdAndRemove(checkedItemId, function(err){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.redirect("/");
-    }
-  });
+  if (listName === "Today"){
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if(err){
+        console.log(err);
+      }
+      else {
+        res.redirect("/");
+      }
+    });
+  }
+  else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if (!err) {
+        res.redirect("/" + listName);
+      }
+    });
+  }
+
+
 });
 
 
